7f419ce328fc6b562a7ee4c06569813bc36a7a48
Manjunath HS <hsmanjunathhsmanjunath@gmail.com>
2024-08-03 13:59:15 +0530

    Added chat history

--- src/app.py
+++ src/app.py
@@ -1,19 +1,30 @@
 import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
 
+def get_response(user_input):
+   return "IDK"
+
+#app config
 st.set_page_config(page_title="Chat With Websites", page_icon="🤖")
-
 st.title("Chat With Websites")
+chat_history = [
+   AIMessage(content="Hello, I am a bot. How can i help you?"),
+]
 
+#sidebar
 with st.sidebar:
     st.header("Settings")
     website_url = st.text_input("Website URL")
     
-st.chat_input("Type Your message here.....")
+#user input
+user_query = st.chat_input("Type Your message here.....")
+if user_query is not None and user_query != "":
+    response = get_response(user_query)
+    chat_history.append(HumanMessage(content=user_query))
+    chat_history.append(AIMessage(content=response))
 
-with st.chat_message("AI"):
-    st.write("Hello, How can i help you")
+with st.sidebar:
+    st.write(chat_history)
 
-with st.chat_message("Human"):
-    st.write("I want to know about Langchain")
 
 
