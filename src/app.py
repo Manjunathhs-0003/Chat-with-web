a85878ad44bc0edfa2da05996730a7d415eeae89
Manjunath HS <hsmanjunathhsmanjunath@gmail.com>
2024-08-03 20:56:20 +0530

    Some update

--- src/app.py
+++ src/app.py
@@ -20,22 +20,28 @@ if "chat_history" not in st.session_state:
 with st.sidebar:
     st.header("Settings")
     website_url = st.text_input("Website URL")
-    
-#user input
-user_query = st.chat_input("Type Your message here.....")
-if user_query is not None and user_query != "":
-    response = get_response(user_query)
-    st.session_state.chat_history.append(HumanMessage(content=user_query))
-    st.session_state.chat_history.append(AIMessage(content=response))
 
-#Conversation
-for message in st.session_state.chat_history:
-    if isinstance(message, AIMessage):
-        with st.chat_message("AI"):
-            st.write(message.content)
-    elif isinstance(message, HumanMessage):
-        with st.chat_message("Human"):
-            st.write(message.content)
+if website_url is None or website_url == "":
+    st.info("Please enter a website URL")
+else:
+    #user input
+    user_query = st.chat_input("Type Your message here.....")
+    if user_query is not None and user_query != "":
+        response = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
+
+    #Conversation
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
+
+    
+
 
 
 
