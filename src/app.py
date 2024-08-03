f58ce77c2116cad243b82519af296f0243c3436d
Manjunath HS <hsmanjunathhsmanjunath@gmail.com>
2024-08-03 13:24:34 +0530

    Frontend done

--- src/app.py
+++ src/app.py
@@ -7,3 +7,13 @@ st.title("Chat With Websites")
 with st.sidebar:
     st.header("Settings")
     website_url = st.text_input("Website URL")
+    
+st.chat_input("Type Your message here.....")
+
+with st.chat_message("AI"):
+    st.write("Hello, How can i help you")
+
+with st.chat_message("Human"):
+    st.write("I want to know about Langchain")
+
+
