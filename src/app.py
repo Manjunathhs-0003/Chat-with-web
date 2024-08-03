f185f8ae946a0ca9fdff9bd46d71c9af5e093fc3
Manjunath HS <hsmanjunathhsmanjunath@gmail.com>
2024-08-03 23:11:44 +0530

    Convert document into chunks

--- src/app.py
+++ src/app.py
@@ -1,15 +1,28 @@
 import streamlit as st
 from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def get_response(user_input):
    return "IDK"
+
+def get_vectorstore_url(url):
+    #get the text in document form
+    loader = WebBaseLoader(url)
+    document = loader.load()
+
+    #split document into chunk
+    text_splitter = RecursiveCharacterTextSplitter()
+    document_chunk = text_splitter.split_documents(document)
+
+    return document_chunk
 
 #app config
 st.set_page_config(page_title="Chat With Websites", page_icon="🤖")
 st.title("Chat With Websites")
 
-''' evrytime we enter a input, everything will be considered as a new cycle,
- in order to maintain a chat history, make it persistent '''
+#evrytime we enter a input, everything will be considered as a new cycle,
+#in order to maintain a chat history, make it persistent
 
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = [
@@ -24,6 +37,8 @@ with st.sidebar:
 if website_url is None or website_url == "":
     st.info("Please enter a website URL")
 else:
+    documents_chunks = get_vectorstore_url(website_url)
+    st.write(documents_chunks)
     #user input
     user_query = st.chat_input("Type Your message here.....")
     if user_query is not None and user_query != "":
@@ -41,6 +56,14 @@ else:
                 st.write(message.content)
 
     
+
+
+
+#HTML documents -> use langchain to extract the texts -> convert text into chunks -> 
+# chunks are passed through the embeddings model -> Embeddings convert these cunks into numerical representations (0's and 1's) -> 
+# store it in vector -> convert user's query to do the same thing -> 
+# then perform the semantic search using vector data to find the data similar to the query -> 
+# send the returned chunk into llm -> find the one that matched the chunk
 
 
 
